f289a869a255e769d8b6093ef1bc585e4ca9cbea
DarkLord76865 <mmima191@gmail.com>
2024-03-28 20:37:27 +0100

    remove bound checks in interpreter

--- src/interpret.rs
+++ src/interpret.rs
@@ -33,31 +33,42 @@ pub fn interpret(token_stream: TokenStream) {
     let mut data_ptr = 0;
     let mut storage = [0_u8; STORAGE_SIZE];
 
-    while ins_ptr < token_stream.len() {
-        match token_stream[ins_ptr] {
-            Token::Add(n) => storage[data_ptr] = storage[data_ptr].wrapping_add(n),
-            Token::Mov(n) => {
-                data_ptr += n;
-                if data_ptr >= STORAGE_SIZE {
-                    data_ptr -= STORAGE_SIZE;
-                }
-            },
-            Token::Input => storage[data_ptr] = getchar(),
-            Token::Output => putchar(storage[data_ptr]),
-            Token::OpenBr(jmp) => {
-                // skip the loop if the current cell is 0
-                if storage[data_ptr] == 0 {
-                    ins_ptr += jmp;
-                }
-            },
-            Token::CloseBr(jmp) => {
-                // return to the start of the loop if the current cell is not 0
-                if storage[data_ptr] != 0 {
-                    ins_ptr -= jmp;
-                }
-            },
-            Token::ClearCell => storage[data_ptr] = 0,
+    // unsafe block because unchecked methods are used to eliminate bounds checks
+    unsafe {
+        while ins_ptr < token_stream.len() {
+            match *token_stream.get_unchecked(ins_ptr) {
+                Token::Add(n) => {
+                    *storage.get_unchecked_mut(data_ptr) = storage.get_unchecked(data_ptr).wrapping_add(n)
+                },
+                Token::Mov(n) => {
+                    data_ptr += n;
+                    if data_ptr >= STORAGE_SIZE {
+                        data_ptr -= STORAGE_SIZE;
+                    }
+                },
+                Token::Input => {
+                    *storage.get_unchecked_mut(data_ptr) = getchar()
+                },
+                Token::Output => {
+                    putchar(*storage.get_unchecked(data_ptr));
+                },
+                Token::OpenBr(jmp) => {
+                    // skip the loop if the current cell is 0
+                    if *storage.get_unchecked(data_ptr) == 0 {
+                        ins_ptr += jmp;
+                    }
+                },
+                Token::CloseBr(jmp) => {
+                    // return to the start of the loop if the current cell is not 0
+                    if *storage.get_unchecked(data_ptr) != 0 {
+                        ins_ptr -= jmp;
+                    }
+                },
+                Token::ClearCell => {
+                    *storage.get_unchecked_mut(data_ptr) = 0
+                },
+            }
+            ins_ptr += 1;
         }
-        ins_ptr += 1;
     }
 }
